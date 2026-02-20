8625fbc811750b5b9078d9b92ef40473621f630c
SmokedSeaSalt <mathijsrijn@gmail.com>
2026-02-20 15:23:32 +0100

    start docker secrets. fix docker volume mount locations

---
+++ srcs/requirements/mariadb/tools/run_mariadb.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir -p /run/mysqld
+
+chown -R mysql:mysql /run/mysqld
+
+# Listen to all ports to be able to receive wordpress requests
+echo "bind-address=0.0.0.0" >> /etc/mysql/my.cnf
+
+mariadbd --user=mysql
+
+# Create user and database for wordpress to use
+mariadb -u root -p "$DB_ROOT_PASSWORD" -e \
+	" \
+	CREATE DATABASE IF NOT EXISTS $DB_NAME; \
+	CREATE USER '$DB_ADMIN_USER'@'%' IDENTIFIED BY '$DB_ADMIN_PASS'; \
+	GRANT ALL PRIVILEGES ON *.* TO '$DB_ADMIN_USER'@'%' WITH GRANT OPTION; \
+	FLUSH PRIVILEGES; \
+	"
