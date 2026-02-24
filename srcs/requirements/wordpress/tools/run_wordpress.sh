7dd8ca11075ce799e06cff71f915d5255656af8c
SmokedSeaSalt <mathijsrijn@gmail.com>
2026-02-24 14:23:05 +0100

    add wp home to port 1443

--- srcs/requirements/wordpress/tools/run_wordpress.sh
+++ srcs/requirements/wordpress/tools/run_wordpress.sh
@@ -28,14 +28,6 @@ rm -rf *
 # Download wp https://developer.wordpress.org/cli/commands/core/download/
 wp core download --locale=en_US --allow-root
 
-# Create config file https://developer.wordpress.org/cli/commands/config/create/
-# wp config create \
-#   --dbname="$DB_NAME" \
-#   --dbuser="$DB_ADMIN_USER" \
-#   --dbpass="$DB_ADMIN_PASS" \
-#   --dbhost="$DB_HOST" \
-#   --allow-root
-
 # Replace wp-config-sample.php with wp-config.php and fill in credentials
 sed \
 	-e "s/database_name_here/$DB_NAME/" \
@@ -44,6 +36,9 @@ sed \
 	-e "s/localhost/$DB_HOST/" \
 	wp-config-sample.php > wp-config.php
 
+# Temp fix for browser redirect to localhost:443
+echo "define('WP_HOME', 'https://localhost:1443'); \
+define('WP_SITEURL', 'https://localhost:1443');" >> wp-config.php
 
 # Install wp https://developer.wordpress.org/cli/commands/core/install/
 wp core install \
@@ -66,9 +61,12 @@ wp user create \
 	--allow-root
 
 
+
 # configure php-fpm to listen to 9000 to communicate with nginx
 sed -i "s#listen = /run/php/php8.2-fpm.sock#listen = 0.0.0.0:9000#" /etc/php/8.2/fpm/pool.d/www.conf
 
 # dpkg -L php-fpm
 # which php-fpm
+echo "Setup done: starting php-fpm"
+
 /usr/sbin/php-fpm8.2 -F
